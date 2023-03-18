baa962acdc91672297901f6b39754de545a6f0cf
JhonatanRian <jhonatanrian.jr@gmail.com>
2023-03-18 12:39:51 -0300

    configurando asgi para o projeto e apagando rounting

--- menseger/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
-from chat.routing import websocket_urlpatterns
-
-application = ProtocolTypeRouter({
-    'websocket': URLRouter(websocket_urlpatterns)
-})
