2ab434f3246c16c874d75bd09d7a7d062afa3cf8
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2023-05-01 10:55:11 -0300

    update

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import IndexView, SalaView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path("chat/<str:room_name>/", SalaView.as_view(), name="room"),
+    path("login/", views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,20 @@
 from django.views.generic import TemplateView
 from django.utils.safestring import mark_safe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Calls
 import json
 
-class IndexView(TemplateView):
+
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context["chats"] = self.request.user.answered_calls.all() if self.request.user.is_staff else self.request.user.calls.all()
+        return context
 
-class SalaView(TemplateView):
+
+class SalaView(LoginRequiredMixin, TemplateView):
     template_name = 'sala.html'
 
     def get_context_data(self, **kwargs):
